1bf48bc98ed1d3931cb1891086e1c5bb9c09fe85
Pauli Marttinen <Gwaur@users.noreply.github.com>
2016-05-21 12:35:49 +0300

    Created variables.php
    This file includes the configuration.

---
+++ variables.php
@@ -0,0 +1,27 @@
+<?
+//$getstoseeall is the IP address who gets to see the hidden relays as well.
+$getstoseeall = '???';
+
+//$prefix is a prefix attached to every username. For example, for Reddit
+//communities, the prefix would be /u/.
+$prefix = '/u/';
+
+//$community is the name of the community in which this relay is played.
+$community = 'Our Cool Subreddit';
+
+//$host is the name of the person who runs the website.
+$host = 'Mr. Meyer';
+
+//$danger is to warn users of an ongoing reprogramming situation and a prospect
+//of bugs
+//$danger = 1;
+
+//the next variables are texts to show when some piece of information is missing
+$nolang = '?';
+$noby = '?';
+$notext = '(text not entered)';
+$notransl = '(translation not entered)';
+$nogloss = '(gloss not entered)';
+$noglossary = '(glossary not entered)';
+$nogrammar = '(grammar not entered)';
+$noipa = '(no IPA entered)';
